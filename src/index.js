9793dd1606777bb4a722852b93fd12425d2d72b1
Davi Banfi Nogueira <davibanfi1579@outlook.com>
2021-09-02 09:18:46 -0300

    feat: method to remove itens in cart

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@
         const $cartItems = document.getElementById('cart-items')
         const $lotteryNumbers = document.getElementsByClassName('bet')
         const $addToCart = document.getElementById('add-to-cart')
-        const $total = document.getElementById('total')
+        const $clear = document.getElementById('clear')
+        const $totalSum = document.getElementById('total-sum')
+        const total = {
+            lotofacil: new Set(),
+            megasena: new Set(),
+            lotomania: new Set()
+        }
 
         return {
             init: function () {
@@ -17,32 +23,39 @@
 
             initEvents: function () {
                 $addToCart.addEventListener('click', () => this.addDivToCart($addToCart), false)
+                $clear.addEventListener('click', () => this.clearChecks(), false)
             },
 
             addNumberToDivCart: function ($option) {
-                $option.children[0].textContent += ', ' + this.getLotteryNumbers()
+                const lotteryType = $option.id.match(/(\w+$)/)[0]
+
+                const newNumbers = this.getLotteryNumbers(lotteryType)
+
+                Array.prototype.forEach.call(newNumbers, (number) => {
+                    total[lotteryType].add(number)
+                })
+
+                $option.children[0].textContent = Array.from(total[lotteryType]).toString()
+
+                this.getTotal()
             },
 
             getTotal: function() {
-                const $1 = document.getElementById('cart-numbers-container-lotofacil')
-                const $2 = document.getElementById('cart-numbers-container-megasena')
-                const $3 = document.getElementById('cart-numbers-container-lotomania')
+                const array = []
 
-                const lotofacil = Array.from($1.children[0].textContent)
-                const megasena = Array.from($2.children[0].textContent)
-                const lotomania = Array.from($3.children[0].textContent)
+                array.push(Array.from(total.lotofacil))
+                array.push(Array.from(total.megasena))
+                array.push(Array.from(total.lotomania))
 
-                console.log(lotofacil.length, megasena.length, lotomania.length)
+                $totalSum.innerText = 'R$ ' + (array.flat(1).length * 0.35).toFixed(2).toString()
             },
 
-            getLotteryNumbers: function () {
-                const numbers = new Set()
-
+            getLotteryNumbers: function (id) {
                 Array.prototype.forEach.call($lotteryNumbers, ($number) => {
-                    $number.checked ? numbers.add(+$number.id) : ''
+                    $number.checked ? total[id].add(+$number.id) : ''
                 })
 
-                return Array.from(numbers).join(', ')
+                return Array.from(total[id])
             },
 
             clearChecks: function () {
@@ -70,8 +83,6 @@
                     return
                 }
 
-                const numbers = app.getLotteryNumbers()
-
                 const $iconAndNumbers = document.createElement('div')
                 const $number = document.createElement('p')
                 const $cartNumbersType = document.createElement('div')
@@ -82,27 +93,42 @@
 
                 $trashIcon.setAttribute('src', './assets/trash_can.png')
                 $trashIcon.setAttribute('alt', 'trash can')
-                $trashIcon.setAttribute('class', 'icon')
+                $trashIcon.setAttribute('class', 'trash-icon')
+
+                $trashIcon.addEventListener('click', () => app.removeCartItem($iconAndNumbers), false)
+
                 $iconAndNumbers.setAttribute('class', 'icon-and-numbers')
 
                 if ($lotofacil.checked) {
+                    const numbers = app.getLotteryNumbers('lotofacil')
+                    $number.innerText = numbers.toString()
+                    $price.innerText = 'R$ ' + (numbers.length * 0.35).toFixed(2).toString()
+
+                    $iconAndNumbers.setAttribute('id', 'icon-and-numbers-lotofacil')
                     $cartNumbersContainer.setAttribute('id', `cart-numbers-container-lotofacil`)
                     $lottery.setAttribute('id', 'lottery-lotofacil')
                     $lottery.innerText = 'Lotofácil'
                 } else if ($megasena.checked) {
+                    const numbers = app.getLotteryNumbers('megasena')
+                    $number.innerText = numbers.toString()
+                    $price.innerText = 'R$ ' + (numbers.length * 0.35).toFixed(2).toString()
+
+                    $iconAndNumbers.setAttribute('id', 'icon-and-numbers-megasena')
                     $cartNumbersContainer.setAttribute('id', `cart-numbers-container-megasena`)
                     $lottery.setAttribute('id', 'lottery-megasena')
                     $lottery.innerText = 'Mega-Sena'
                 } else if ($lotomania.checked) {
+                    const numbers = app.getLotteryNumbers('lotomania')
+                    $number.innerText = numbers.toString()
+                    $price.innerText = 'R$ ' + (numbers.length * 0.35).toFixed(2).toString()
+
+                    $iconAndNumbers.setAttribute('id', 'icon-and-numbers-lotomania')
                     $cartNumbersContainer.setAttribute('id', `cart-numbers-container-lotomania`)
                     $lottery.setAttribute('id', 'lottery-lotomania')
                     $lottery.innerText = 'Lotomania'
                 }
 
                 $cartNumbersType.setAttribute('class', 'cart-numbers-type')
-
-                $number.innerText = numbers.toString()
-                $price.innerText = 'R$ ' + (numbers.length * 0.50).toString()
 
                 $cartNumbersType.appendChild($lottery)
                 $cartNumbersType.appendChild($price)
@@ -114,7 +140,17 @@
 
                 app.getTotal()
                 app.clearChecks()
-            }
+            },
+
+            removeCartItem: function($iconAndNumbers) {
+                $iconAndNumbers.remove()
+
+                const lotteryType = $iconAndNumbers.id.match(/(\w+$)/)[0]
+
+                total[lotteryType].clear()
+
+                app.getTotal()
+            },
         }
     })()
 
